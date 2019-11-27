568d5fd9c5bce866a9537d5d63d129ff07c0405c
geoagdt <geoagdt@10.9.146.117>
2019-11-27 11:03:56 +0000

    Code ratonalisation.

--- src/main/java/uk/ac/leeds/ccg/andyt/data/format/Data_ReadTXT.java
+++ src/main/java/uk/ac/leeds/ccg/andyt/data/format/Data_ReadTXT.java
@@ -28,10 +28,13 @@ import uk.ac.leeds.ccg.andyt.data.core.Data_Environment;
 import uk.ac.leeds.ccg.andyt.data.core.Data_Object;
 
 /**
- * For reading ASCII text files (such as CSV format files) into a collection of
- * Strings (one for each line in the file).
+ * For reading ASCII text files (such as CSV format files). {@link #read} is for
+ * reading an entire file into a collection of Strings (one for each line in the
+ * file). {@link #readLine(java.io.StreamTokenizer)} reads once a
+ * {@link java.io.Reader} and {@link java.io.StreamTokenizer} have ben set up.
  *
- * @author geoagdt
+ * @author Andy Turner
+ * @version 1.0.0
  */
 public class Data_ReadTXT extends Data_Object {
 
@@ -56,16 +59,14 @@ public class Data_ReadTXT extends Data_Object {
         File test = new File(testDir, "test" + syntax + ".csv");
         PrintWriter pw = env.env.io.getPrintWriter(f, false);
         if (f.exists()) {
-            BufferedReader br = env.env.io.getBufferedReader(f);
-            if (br != null) {
+            try (BufferedReader br = env.env.io.getBufferedReader(f)) {
                 StreamTokenizer st = new StreamTokenizer(br);
                 setStreamTokenizerSyntax(st, syntax);
                 int token = st.nextToken();
                 while (!(token == StreamTokenizer.TT_EOF)) {
-                    readLine(st, pw);
+                    pw.println(readLine(st));
                     token = st.nextToken();
                 }
-                br.close();
             }
             pw.close();
         } else {
@@ -86,6 +87,12 @@ public class Data_ReadTXT extends Data_Object {
         //return result;
     }
 
+    /**
+     * A convenience method for setting the StreamTokensizer syntax.
+     *
+     * @param st The StreamTokensizer that's parsing syntax is set.
+     * @param syntax Currently expecting values 1 to 7 inclusive.
+     */
     protected void setStreamTokenizerSyntax(StreamTokenizer st, int syntax) {
         switch (syntax) {
             case 1:
@@ -124,99 +131,33 @@ public class Data_ReadTXT extends Data_Object {
      * @param testDir If this is null then a test file is not written out.
      * Otherwise a test file is written out in testDir
      * @param syntax For setting the syntax of the StreamTokenizer (see for
-     * example, Generic_IO.setStreamTokenizerSyntax1(st))
+     * example, Generic_IO.setStreamTokenizerSyntax1(st)). Currently expecting
+     * values 1 to 7 inclusive.
      * @return A list of Strings, one for each line of {@code f}.
      * @throws java.io.IOException If there is an IO issue!
      */
     public ArrayList<String> read(File f, File testDir, int syntax)
             throws IOException {
-        ArrayList<String> result = null;
-        PrintWriter pw = null;
-        // Initialise pw
-        if (testDir != null) {
+        if (testDir.exists()) {
             testDir.mkdirs();
-            File test = new File(testDir, "test" + syntax + ".csv");
-            pw = new PrintWriter(test);
-        }
-
-        if (f.exists()) {
-            BufferedReader br = env.env.io.getBufferedReader(f);
-            if (br != null) {
-                result = new ArrayList<>();
+            File outf = new File(testDir, "test" + syntax + ".csv");
+            PrintWriter pw = new PrintWriter(outf);
+            ArrayList<String> r = new ArrayList<>();
+            try (BufferedReader br = env.env.io.getBufferedReader(f)) {
                 StreamTokenizer st = new StreamTokenizer(br);
                 setStreamTokenizerSyntax(st, syntax);
-                int token = st.nextToken();
-//                    long RecordID = 0;
-                if (pw == null) {
-                    while (!(token == StreamTokenizer.TT_EOF)) {
-                        result.add(readLine(st, pw));
-                        token = st.nextToken();
-                    }
-                } else {
-                    while (!(token == StreamTokenizer.TT_EOF)) {
-                        result.add(readLine(st));
-                        token = st.nextToken();
-                    }
-                }
-                br.close();
-            }
-            if (pw != null) {
-                pw.close();
-            }
-        } else {
-            System.out.println("File " + f + " does not exist!");
-        }
-        return result;
-    }
-
-    /**
-     * Read and return a line of the text file or null if the file is all read.
-     *
-     * @param st The StreamTokenizer for parsing the stream.
-     * @param pw If this is not null then the line returned is printed to it.
-     * @return The next line or null.
-     * @throws java.io.IOException If there is an IO issue!
-     */
-    public String readLine(StreamTokenizer st, PrintWriter pw)
-            throws IOException {
-        int token = st.nextToken();
-        String line = "";
-        while (!(token == StreamTokenizer.TT_EOF)) {
-            switch (token) {
-                case StreamTokenizer.TT_EOL:
+                String line = readLine(st);
+                while (line != null) {
+                    r.add(line);
                     pw.println(line);
-                    return line;
-                case StreamTokenizer.TT_WORD:
-                    line += st.sval;
-                    break;
-                case StreamTokenizer.TT_NUMBER:
-                    break;
-                default:
-                    line = doDefault(token, line);
-                    break;
+                    line = readLine(st);
+                }
             }
-            token = st.nextToken();
+            return r;
+        } else {
+            throw new IOException("testDir " + testDir + " does not exist in "
+                    + this.getClass().getName() + ".read(File,File,int)");
         }
-        return null;
-    }
-
-    private String doDefault(int token, String line) {
-        if (token == 26 || token == 160) {
-            /**
-             * A type of space " ". It is unusual as st probably already set to
-             * parse space as words.
-             */
-            line += (char) token;
-        }
-        if (token == 13) {
-            /**
-             * These are returns or tabs or something...
-             */
-            //line += (char) token;
-        }
-        //System.out.println("line so far " + line);
-        //System.out.println("Odd token " + token + " \"" + (char) token + "\" encountered.");
-        return line;
     }
 
     /**
@@ -237,11 +178,23 @@ public class Data_ReadTXT extends Data_Object {
                 case StreamTokenizer.TT_NUMBER:
                     break;
                 default:
-                    line = doDefault(token, line);
+                    if (token == 26 || token == 160) {
+                        /**
+                         * A type of white-space.
+                         */
+                        line += (char) token;
+                    }
+                    if (token == 13) {
+                        /**
+                         * A type of white-space.
+                         */
+                        //line += (char) token;
+                    }
                     break;
             }
             token = st.nextToken();
         }
         return null;
     }
+
 }
