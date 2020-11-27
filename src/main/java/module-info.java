e33348a2302e8542c45a159cf838ec8b9c596163
agdtu <agdtu@DESKTOP-S5UTMD4>
2020-11-27 07:24:41 +0000

    First modularization and upgrade to latest Math.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module uk.ac.leeds.ccg.projects.fire {
+    requires transitive java.logging;
+    requires java.desktop;
+    requires uk.ac.leeds.ccg.generic;
+    requires uk.ac.leeds.ccg.math;
+}
