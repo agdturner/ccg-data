8b733973e97071f5fb2216f9a75d09dfb9bae51d
agdtu <agdtu@DESKTOP-S5UTMD4>
2020-11-27 07:27:19 +0000

    First modularization and upgrade to latest Math.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,4 +3,11 @@ module uk.ac.leeds.ccg.projects.fire {
     requires java.desktop;
     requires uk.ac.leeds.ccg.generic;
     requires uk.ac.leeds.ccg.math;
+    exports uk.ac.leeds.ccg.data;
+    exports uk.ac.leeds.ccg.data.converter;
+    exports uk.ac.leeds.ccg.data.core;
+    exports uk.ac.leeds.ccg.data.format;
+    exports uk.ac.leeds.ccg.data.id;
+    exports uk.ac.leeds.ccg.data.interval;
+    exports uk.ac.leeds.ccg.data.io;
 }
